23db2fbd3a05d8ca74c5bae04971bf15f0259a6f
aditipatelpro <aditipatelpro@gmail.com>
2022-01-09 18:59:40 -0500

    articles added

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,26 @@
 import Head from 'next/head'
+import ArticleList from '../components/ArticleList'
 
-export default function Home() {
+export default function Home({ articles }) {
   return (
     <div>
       <Head>
         <title> News Blog</title>
         <meta name='keywords' content='web development, coding, programming'/>
       </Head>
-      <h1> Welcome to the page</h1>
+
+      <ArticleList articles={articles}/>
     </div>
   )
+}
+
+export const getStaticProps = async () => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=6`)
+  const articles = await res.json()
+
+  return {
+    props: {
+      articles,
+    },
+  }
 }
