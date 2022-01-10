1dcd07aca19e686b6de0a470008a829858e37423
aditipatelpro <aditipatelpro@gmail.com>
2022-01-09 22:15:11 -0500

    api setup

---
+++ pages/api/articles/[id].js
@@ -0,0 +1,12 @@
+import { articles } from '../../../data'
+import article from '../../article/[id]'
+
+export default function handler({query: { id }}, res) {
+    const filtered = articles.filter((article) => article.id === id)
+
+    if (filtered.length > 0) {
+        res.status(200).json(filtered[0])
+    } else {
+        res.status(404).json({message: `Article ${id} not found!`})
+    }
+  }
